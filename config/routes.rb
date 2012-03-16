79277f5b1154bba1d216186465889115f9fbee9c
rockyjohnson <samnish1@umbc.edu>
2012-03-15 21:46:07 -0400

    Finished first cut of the User model

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
 root :to => 'pages#home'
+resources :users
+
+  match '/signup',  :to => 'users#new'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
